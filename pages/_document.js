0e50542b70c2f6b811965498ef2fca1bb46175d7
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2025-11-29 09:13:50 +0100

    updated page

--- pages/_document.js
+++ pages/_document.js
@@ -4,37 +4,56 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Tag Manager */}
+        {/* TikTok Pixel */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-52WCT7DC');
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject = t;
+                var ttq = w[t] = w[t] || [];
+                ttq.methods = [
+                  "page","track","identify","instances","debug","on","off","once",
+                  "ready","alias","group","enableCookie","disableCookie",
+                  "holdConsent","revokeConsent","grantConsent"
+                ];
+                ttq.setAndDefer = function (t, e) {
+                  t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments,0))); };
+                };
+                for (var i = 0; i < ttq.methods.length; i++) {
+                  ttq.setAndDefer(ttq, ttq.methods[i]);
+                }
+                ttq.instance = function (t) {
+                  for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) {
+                    ttq.setAndDefer(e, ttq.methods[n]);
+                  }
+                  return e;
+                };
+                ttq.load = function (e, n) {
+                  var r = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                  ttq._i = ttq._i || {};
+                  ttq._i[e] = [];
+                  ttq._i[e]._u = r;
+                  ttq._t = ttq._t || {};
+                  ttq._t[e] = +new Date;
+                  ttq._o = ttq._o || {};
+                  ttq._o[e] = n || {};
+                  var s = document.createElement("script");
+                  s.type = "text/javascript";
+                  s.async = true;
+                  s.src = r + "?sdkid=" + e + "&lib=" + t;
+                  var f = document.getElementsByTagName("script")[0];
+                  f.parentNode.insertBefore(s, f);
+                };
+
+                ttq.load('D4LALDRC77U7MI8IMD80'); 
+                ttq.page();
+              }(window, document, 'ttq');
             `,
           }}
         />
-        {/* End Google Tag Manager */}
       </Head>
 
       <body className="antialiased">
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-52WCT7DC"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         <Main />
         <NextScript />
       </body>
