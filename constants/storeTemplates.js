0ffa87280a9cb990bde6bb42e69dd3771b5e2f67
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2026-07-09 11:57:54 +0100

    edited a feature

--- constants/storeTemplates.js
+++ constants/storeTemplates.js
@@ -7,6 +7,21 @@ const storefrontTemplates = [
     previewUrl: "/p/dvrx-store",
   },
   {
+    id: "6a4cc45e6c29ba32965d40c7",
+    title: "AnkaraMart",
+    image:
+      "https://pub-e0ff826b0e9843069a4df3548bc6fa39.r2.dev/tiklink-pages/1783593040596-7949aa41-3ad2-4093-98f9-e9d3c387b467",
+    previewUrl: "/p/ankaramart",
+  },
+
+  {
+    id: "6a4e79b76c29ba3296600db0",
+    title: "Luxe Wigs",
+    image:
+      "https://pub-e0ff826b0e9843069a4df3548bc6fa39.r2.dev/tiklink-pages/1783593326569-7606db0d-de59-48e4-8db5-e1be820ab302",
+    previewUrl: "/p/luxewigs",
+  },
+  {
     id: "6a10d4f334628f09dde3a5b1",
 
     title: "Green Modern",
@@ -37,22 +52,6 @@ const storefrontTemplates = [
       "https://pub-e0ff826b0e9843069a4df3548bc6fa39.r2.dev/tiklink-pages/1779497957303-48826baf-f7d3-4092-b62e-30fa205a55ae",
 
     previewUrl: "/p/digimax",
-  },
-
-  {
-    id: "6a4cc45e6c29ba32965d40c7",
-    title: "AnkaraMart",
-    image:
-      "https://pub-e0ff826b0e9843069a4df3548bc6fa39.r2.dev/tiklink-pages/1783593040596-7949aa41-3ad2-4093-98f9-e9d3c387b467",
-    previewUrl: "/p/ankaramart",
-  },
-
-  {
-    id: "6a4e79b76c29ba3296600db0",
-    title: "Luxe Wigs",
-    image:
-      "https://pub-e0ff826b0e9843069a4df3548bc6fa39.r2.dev/tiklink-pages/1783593326569-7606db0d-de59-48e4-8db5-e1be820ab302",
-    previewUrl: "/p/luxewigs",
   },
 ];
 
